d952cf65628c5c28c818a57f04ec710f3b4004bc
Aleksandr <kuraga.dev@gmail.com>
2025-03-23 16:32:04 +0300

    PlayerController & InputSystem
    Edited playerController script, added and configured player input component. Changed grass tlies on scene.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
-        
+        MoveCharacter();
+    }
+
+    public void MoveCharacter()
+    {
+        rb.velocity = moveInput.normalized * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
