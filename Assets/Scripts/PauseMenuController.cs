9d36173f20b9be4e489fb7c1d8ce1b9e33b7bb56
Aleksandr <kuraga.dev@gmail.com>
2025-04-14 20:41:07 +0300

    In-game menu
    Added initial version of in-game menu(canvas + buttons + panels), added pause while opened.

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject saveLoadPanel;
+
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (isPaused) Resume();
+            else Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+        saveLoadPanel.SetActive(false);
+    }
+
+    public void OpenSaveLoadMenu()
+    {
+        saveLoadPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void BackToPauseMenu()
+    {
+        saveLoadPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
+    public void QuitToMainMenu()
+    {
+       /* Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");*/
+    }
+}
